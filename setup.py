57c95103bcda05b40656eb6c20b4c1ab6199c343
CSRedRat <csredrat@gmail.com>
2020-10-31 17:30:48 +0500

    Bump version: 1.1.3 → 1.1.4

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/CSRedRat/topalias",
-    version="1.1.3",
+    version="1.1.4",
     zip_safe=False,
 )
--- topalias/__init__.py
+++ topalias/__init__.py
@@ -3,6 +3,6 @@
 
 __author__ = "Sergey Chudakov"
 __email__ = "csredrat@gmail.com"
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 
 from topalias import *
