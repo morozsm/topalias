70a0493990adb723457ded7a467125f3b674a866
CSRedRat <csredrat@gmail.com>
2020-11-19 17:44:45 +0500

    Bump version: 2.0.9 → 2.0.10

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/CSRedRat/topalias",
-    version="2.0.9",
+    version="2.0.10",
     zip_safe=False,
 )
