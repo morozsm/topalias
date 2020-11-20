c75bb70259232d4947d19c7d077033271c5b21ed
Sergey Morozik <morozsm@gmail.com>
2020-11-20 11:18:06 -0500

    fix WPS327

--- topalias/aliascore.py
+++ topalias/aliascore.py
@@ -237,12 +237,10 @@ def load_command_bank(filtering=False):  # pylint: disable=too-many-branches
                         multiline_buffer.append(line.strip())
                     # Check if we have multiline in buffer
                     if multiline_buffer:
-                        line = " ".join(multiline_buffer) # noqa WPS440
+                        line = " ".join(multiline_buffer)  # noqa WPS440
                         multiline_buffer = []
                     if process_zsh_line(line, filtering):
                         command_bank.append(process_zsh_line(line, filtering))
-                    else:
-                        continue
 
     except FileNotFoundError:
         print("Try: topalias -f /path/to/home/folder/with/history")
